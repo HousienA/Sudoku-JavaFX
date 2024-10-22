1868b3efb7e46e5620444996681d6a86d7c82068
Ari0115 <arihumvarper@gmail.com>
2024-10-22 18:35:06 +0200

    Added SudokuView.java

--- Sudoku/src/main/java/housienariel/SudokuCell.java
+++ Sudoku/src/main/java/housienariel/SudokuCell.java
@@ -2,9 +2,28 @@ package housienariel;
 
 public class SudokuCell {
 
-  int correctValue;
-  int userInput;
+  int value;
   boolean isLocked;
 
-  
+  public SudokuCell(){
+    this.value = 0;
+    this.isLocked = false;
+  }
+
+  public int getvalue() {
+    return value;
+  }
+
+  public void setvalue(int value) {
+    this.value = value;
+  }
+
+  public boolean isLocked() {
+    return isLocked;
+  }
+
+  public void setLocked(boolean locked) {
+   this.isLocked = locked;
+  }
+
 }
---
+++ Sudoku/src/main/java/housienariel/SudokuView.java
@@ -0,0 +1,5 @@
+package housienariel;
+
+public class SudokuView {
+
+}
